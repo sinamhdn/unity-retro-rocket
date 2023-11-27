752016a217a0052f733d85032d67442a82220321
SinaMHDN <sina.mh.id@gmail.com>
2023-11-27 06:18:30 +0330

    Added thrust audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,27 +4,39 @@ public class Rocket : MonoBehaviour
 {
     [SerializeField] float rocketMass = 0.1f;
     Rigidbody rigidbody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Throttle();
+        Rotate();
     }
 
-    void ProcessInput()
+    void Throttle()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.mass = rocketMass;
             rigidbody.AddRelativeForce(Vector3.up);
+            // we add if because when we hold space it will try to play again and again
+            if (!audioSource.isPlaying) audioSource.Play();
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
 
+    void Rotate()
+    {
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward * Time.deltaTime);
